d856041352f1926af52c8525e8ad74dbbd4f8a98
Taylor Sargent <sarg0049@algonquinlive.com>
2016-02-28 21:44:38 -0500

    added variables to the js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,49 @@
-var newLetter = prompt('Enter a letter:');
+var newStory1 = prompt('Lets make a story together!');
 
-var i = 0;
+var newStory2 = prompt('A colour (orange, indigo,etc.)');
 
-var j = 0;
+var i = 0
 
-for (i = 0; i < 10; i++) {
-  for (j = 0; j < i; j++) {
-    document.write(newLetter);
-  }
-  document.write('<br>');
+for (i = 0; i < 1; i++) {
+  document.write(newStory2 );
+}
+
+var newStory3 = prompt('A creature (dragon, moth, etc.)');
+
+var j = 0
+
+for (j = 0; j < 1; j++) {
+  document.write(newStory3 );
+}
+
+var newStory4 = prompt('An adjective (beautiful, super, etc.)');
+
+var k = 0
+
+for (k = 0; k < 1; k++) {
+  document.write(newStory4 );
+}
+
+var newStory5 = prompt('A past tense verb (ran, burped, etc.)');
+
+var l = 0
+
+for (l = 0; l < 1; l++) {
+  document.write(newStory5 );
+}
+
+var newStory6 = prompt('Which story would you like?');
+
+var m = 0
+
+if (newStory6 == 'Snacking') {
+  document.write('After snacking on (newStory4) treats, the (newStory2) bellied (newStory3) (newStory5) for hours');
+}
+
+if (newStory6 == 'Escaping') {
+  document.write('After escaping on (newStory4) treats, the (newStory2) bellied (newStory3) (newStory5) for hours');
+}
+
+if (newStory6 == 'Winning') {
+  document.write('After winning on (newStory4) treats, the (newStory2) bellied (newStory3) (newStory5) for hours');
 }
